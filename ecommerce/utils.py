2cb35cd17a362330c481df4fbdf7b5bbe5247ee3
Madanshrestha <madan.stha3@gmail.com>
2018-05-29 13:34:00 +0545

    new app order

--- ecommerce/utils.py
+++ ecommerce/utils.py
@@ -3,12 +3,13 @@ import string
 
 from django.utils.text import slugify
 
+
 def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
 def unique_slug_generator(instance, new_slug=None):
-    
+
     if new_slug is not None:
         slug = new_slug
     else:
